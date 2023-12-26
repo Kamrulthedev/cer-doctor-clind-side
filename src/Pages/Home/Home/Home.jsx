6d1343b78d531cfe9baa6a0cb837b3ec2374a8d5
Kamrul Hassan <kamrulthedev@gmail.com>
2023-12-26 22:27:23 +0600

    update-commit

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import About from "../About/About";
 import Banner from "../Banner/Banner";
+import Services from "../Services/Services";
 
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div className="max-w-[940px] mr-12">
             <Banner></Banner>
             <About></About>
+            <Services></Services>
         </div>
     );
 };
--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -2,8 +2,12 @@
 
 const Services = () => {
     return (
-        <div>
-            
+        <div className="mt-10">
+            <div className="text-center">
+                <h1 className="text-red-500 font-bold">Service</h1>
+                <h2 className="text-3xl font-bold mb-4"> Our Service Area</h2>
+                <p>the majority have suffered alteration in some form, br <br /> by injected humour, or randomised words which do not look even slightly believable. </p>
+            </div>
         </div>
     );
 };
