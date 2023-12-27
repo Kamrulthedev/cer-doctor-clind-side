feaa6d6e3020a704835df3f656f15f2e16c8563a
Kamrul Hassan <kamrulthedev@gmail.com>
2023-12-27 13:40:47 +0600

    igm-commit

--- src/Pages/Home/Prodect/Prodect.jsx
+++ src/Pages/Home/Prodect/Prodect.jsx
@@ -2,8 +2,15 @@
 
 const Prodect = () => {
     return (
-        <div>
-            
+        <div className="mt-10 mb-11">
+            <div className="text-center "> 
+                <p className="text-red-500 font-bold mb-2">Popular Products</p>
+                <h1 className="text-2xl font-bold mb-4">Browse Our Products</h1>
+                <p>the majority have suffered alteration in some form, by injected humour,  <br /> or randomised words which do not look even slightly believable. </p>
+            </div>
+            <div>
+
+            </div>
         </div>
     );
 };
