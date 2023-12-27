02c7ce1dbf34771b6783dfa9c0da1cc1710ba3e5
Kamrul Hassan <kamrulthedev@gmail.com>
2023-12-27 10:54:20 +0600

    local-commit

--- src/fierbase/Fierbase.config.js
+++ src/fierbase/Fierbase.config.js
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC1c5FONotDvV44C91ibSy7PNU7sIr2iEo",
-  authDomain: "cers-doctor.firebaseapp.com",
-  projectId: "cers-doctor",
-  storageBucket: "cers-doctor.appspot.com",
-  messagingSenderId: "4362293888",
-  appId: "1:4362293888:web:74486b53fc8019cdce8c65"
+apiKey:import.meta.env.VITE_APIKEY,
+authDomain:import.meta.env.VITE_AUTHDOMAIN,
+projectId:import.meta.env.VITE_PROJECTID,
+storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
